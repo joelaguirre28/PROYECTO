d773a91afc8f999ccdc297badf69f9bb52d92c15
joelaguirre28 <160807773+joelaguirre28@users.noreply.github.com>
2024-03-06 22:12:09 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-console.log("Hola mundo")
+
 const URL = "https://fakestoreapi.com/products";
+
+let titulo = document.getElementById("titulo")
+let precio = document.getElementById("precio")
+console.log()
+
+
 function getData(){
     fetch(URL)
     .then( response => response.json() )
-    .then( data => console.log("DATOS DE LA API", data))
+    .then( data => {
+        console.log("Datos de la api: ", data)
+        for(let i = 0; i <  data.length; i++ ){
+            console.log(data[i].title)
+            
+        }
+
+        titulo.innerHTML = data[2].title;
+        precio.innerHTML = data[2].price;
+
+})
+
+
+// titulo.innerHTML = "nuevo texto"
+
 }
 
 getData();
+
